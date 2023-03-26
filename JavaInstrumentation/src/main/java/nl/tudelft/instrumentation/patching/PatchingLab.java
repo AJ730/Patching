456e7b33b736efa2543f56e4fcf59ed9b5455b51
YuqianSong7 <125743687+YuqianSong7@users.noreply.github.com>
2023-03-26 01:12:41 +0100

    Implemented crossover

--- JavaInstrumentation/src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
+++ JavaInstrumentation/src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
@@ -15,14 +15,14 @@ public class PatchingLab {
     static HashMap<String, HashSet<Integer>> opsMap;
     static HashMap<Integer, String[]> population;
     static HashMap<Double, String[]> topSelection;
-    static HashMap<Integer, String[]> crossover;
+    static ArrayList<String[]> crossover;
 
     static void initialize() {
         // initialize the population based on OperatorTracker.operators
         opsMap = new HashMap<>();
         population = new HashMap<>();
         topSelection = new HashMap<>();
-        crossover = new HashMap<>();
+        crossover = new ArrayList<String[]>();
     }
 
     // encounteredOperator gets called for each operator encountered while running tests
@@ -97,6 +97,26 @@ public class PatchingLab {
         }
     }
 
+    static void populationCrossover() {
+        int cutoff = r.nextInt(OperatorTracker.operators.length);
+        List<Double> l = new ArrayList<Double>(topSelection.keySet());
+        int limiter = topSelection.size()-1;
+        for (int i = 0; i<limiter; i++) {
+            int j =  i+1;
+            List<String> parent1 = Arrays.asList(topSelection.get(l.get(i)));
+            List<String> parent2 = Arrays.asList(topSelection.get(l.get(j)));
+            ArrayList<String> sub1 = new ArrayList<>();
+            sub1.addAll(parent1.subList(0,cutoff));
+            sub1.addAll(parent2.subList(cutoff,parent2.size()));
+            ArrayList<String> sub2 = new ArrayList<>();
+            sub2.addAll(parent2.subList(0,cutoff));
+            sub2.addAll(parent1.subList(cutoff,parent1.size()));
+            String[] child1 = sub1.toArray(new String[0]);
+            String[] child2 = sub2.toArray(new String[0]);
+            crossover.add(child1);
+            crossover.add(child2);
+        }
+    }
 
     static void run() {
         initialize();
@@ -119,6 +139,8 @@ public class PatchingLab {
             System.out.println("Population: "+population);
             populationSelection(3);
             System.out.println("Selection: "+topSelection);
+            populationCrossover();
+            System.out.print("Crossover: "+ crossover);
             break;
         }
     }
