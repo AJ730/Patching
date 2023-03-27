b5c34bb875a7884e6590d0b087016dde47dd699f
YuqianSong7 <125743687+YuqianSong7@users.noreply.github.com>
2023-03-27 21:08:33 +0200

    Bad code, fixed crossover and implemented mutation

--- JavaInstrumentation/src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
+++ JavaInstrumentation/src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
@@ -16,13 +16,19 @@ public class PatchingLab {
     static HashMap<Integer, String[]> population;
     static HashMap<Double, String[]> topSelection;
     static ArrayList<String[]> crossover;
+    static ArrayList<String[]> mutated;
+    static HashMap<Double, String[]> mutationScores;
+    static String[] bestResult;
+    static final String[] opsString = new String[]{"==", "!=", "<=", "<", ">", ">="};
 
     static void initialize() {
         // initialize the population based on OperatorTracker.operators
         opsMap = new HashMap<>();
         population = new HashMap<>();
         topSelection = new HashMap<>();
-        crossover = new ArrayList<String[]>();
+        crossover = new ArrayList<>();
+        mutated = new ArrayList<>();
+        mutationScores = new HashMap<>();
     }
 
     // encounteredOperator gets called for each operator encountered while running tests
@@ -89,7 +95,7 @@ public class PatchingLab {
             double fitnessValSelection = getFitness(selection);
             topSelection.put(fitnessValSelection, population.get(i));
         }
-        List<Double> l = new ArrayList<Double>(topSelection.keySet());
+        List<Double> l = new ArrayList<>(topSelection.keySet());
         Collections.sort(l);
         int limiter = topSelection.size()-sizeLim;
         for (int j= 0; j< limiter; j++){
@@ -99,23 +105,61 @@ public class PatchingLab {
 
     static void populationCrossover() {
         int cutoff = r.nextInt(OperatorTracker.operators.length);
-        List<Double> l = new ArrayList<Double>(topSelection.keySet());
+        List<Double> l = new ArrayList<>(topSelection.keySet());
         int limiter = topSelection.size()-1;
         for (int i = 0; i<limiter; i++) {
-            int j =  i+1;
-            List<String> parent1 = Arrays.asList(topSelection.get(l.get(i)));
-            List<String> parent2 = Arrays.asList(topSelection.get(l.get(j)));
-            ArrayList<String> sub1 = new ArrayList<>();
-            sub1.addAll(parent1.subList(0,cutoff));
-            sub1.addAll(parent2.subList(cutoff,parent2.size()));
-            ArrayList<String> sub2 = new ArrayList<>();
-            sub2.addAll(parent2.subList(0,cutoff));
-            sub2.addAll(parent1.subList(cutoff,parent1.size()));
-            String[] child1 = sub1.toArray(new String[0]);
-            String[] child2 = sub2.toArray(new String[0]);
-            crossover.add(child1);
-            crossover.add(child2);
+            for (int j =1; j<=limiter; j++) {
+                if (i!=j) {
+                    List<String> parent1 = Arrays.asList(topSelection.get(l.get(i)));
+                    List<String> parent2 = Arrays.asList(topSelection.get(l.get(j)));
+                    ArrayList<String> sub1 = new ArrayList<>();
+                    sub1.addAll(parent1.subList(0, cutoff));
+                    sub1.addAll(parent2.subList(cutoff, parent2.size()));
+                    ArrayList<String> sub2 = new ArrayList<>();
+                    sub2.addAll(parent2.subList(0, cutoff));
+                    sub2.addAll(parent1.subList(cutoff, parent1.size()));
+                    String[] child1 = sub1.toArray(new String[0]);
+                    String[] child2 = sub2.toArray(new String[0]);
+                    crossover.add(child1);
+                    crossover.add(child2);
+                }
+            }
         }
+    }
+
+    static void populationMutation(double treshold) {
+        // This part loops through the crossover children and mutates them
+        for (String[] ele : crossover){
+            HashMap<String, Double> tarscores = new HashMap<>();
+            List<String> ops = Arrays.asList(ele);
+            OperatorTracker.operators = ele;
+            List<Boolean> mutation = OperatorTracker.runAllTests();
+            for (String s: opsMap.keySet()){
+                tarscores.put(s,tarantulaScore(s, mutation));
+            }
+            for (String t: tarscores.keySet()){
+                if (tarscores.get(t) > treshold){
+                    String mutated = opsString[r.nextInt(opsString.length)];
+                    for (int i = 0; i<ops.size(); i++){
+                        if (ops.get(i).equals(t)){
+                            float chance = r.nextFloat();
+                            if (chance<0.05){
+                                ops.set(i,mutated);
+                            }
+                        }
+                    }
+                }
+            }
+            // Calculates the fitness of the mutated stuff and adds them to mutationScores
+            String[] brokencode = ops.toArray(new String[0]);
+            OperatorTracker.operators = brokencode;
+            List<Boolean> fitnessOfMutated = OperatorTracker.runAllTests();
+            mutationScores.put(getFitness(fitnessOfMutated), brokencode);
+        }
+        // Dumb method to add the highest fitness scoring result to the bestresult
+        List<Double> l = new ArrayList<>(mutationScores.keySet());
+        Collections.sort(l);
+        bestResult = mutationScores.get(l.get(l.size()-1));
     }
 
     static void run() {
@@ -140,7 +184,12 @@ public class PatchingLab {
             populationSelection(3);
             System.out.println("Selection: "+topSelection);
             populationCrossover();
-            System.out.print("Crossover: "+ crossover);
+            System.out.println("Crossover: "+ crossover);
+            populationMutation(0.50);
+            System.out.println("Result of Mutation" + mutationScores);
+            OperatorTracker.operators = bestResult;
+            double fitnessVal1 = getFitness(OperatorTracker.runAllTests());
+            System.out.println("New Fitness Value after a run = " + fitnessVal1);
             break;
         }
     }
